09c411169c8a645402c4d8b1599d64820554ab94
devmfundo <devmfundo@gmail.com>
2024-08-21 23:31:10 +0200

    added currentYear: new date().getFullYear on my footer <p> so i wont have to update my date manually

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,9 @@
 export default function footer(){
+    const currentYear = new Date().getFullYear();
     const footer = document.createElement("footer");
     footer.id="footer";
     document.body.append(footer);
     const footerP = document.createElement("p");
-    footerP.innerHTML="&copy; 2024 African Elegance Restaurant. All Rights Reserved.";
+    footerP.innerHTML=`&copy; ${currentYear} African Elegance Restaurant. All Rights Reserved.`;
     footer.appendChild(footerP);
 }
